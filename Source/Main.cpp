011dc2ec8df044946bd543f3ff4bb298763ba6db
Othereum <seokjin.dev@gmail.com>
2020-06-06 15:53:39 +0900

    feat: Draw cube

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,14 +1,35 @@
-#include "Engine.h"
-#include "GameModule.hpp"
+#include "Engine.hpp"
+#include "Log.hpp"
 
-namespace oeng
+#include "Actor.hpp"
+#include "Components/MeshComponent.hpp"
+
+using namespace oeng;
+
+static void LoadGame(Engine& e)
 {
-	const char* GetGameName() noexcept
+	auto& world = e.GetWorld();
+	auto& cube = world.SpawnActor<AActor>();
+	auto& cube_mesh = cube.AddComponent<MeshComponent>();
+	cube_mesh.SetMesh("../Engine/Assets/Cube.omesh");
+	cube.SetRootComponent(&cube_mesh);
+	cube.SetTransform({
+		{200, 75, 0},
+		Quat{UVec3::up, 225_deg} * Quat{UVec3::right, 90_deg},
+		{All{}, 100}}
+	);
+}
+
+int main()
+{
+	try
 	{
-		return "Test Game";
+		Engine engine{"Test Game", &LoadGame};
+		engine.RunLoop();
 	}
-	
-	void LoadGameModule(CEngine& engine)
+	catch (const std::exception& e)
 	{
+		log::Critical("Unhandled exception: {}", e.what());
+		throw;
 	}
 }
