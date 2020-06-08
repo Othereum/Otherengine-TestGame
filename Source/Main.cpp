e7370d40939641f7d3de554a18c7700cba4b794a
Othereum <seokjin.dev@gmail.com>
2020-06-08 12:40:05 +0900

    feat: Cube and Sphere

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -9,15 +9,22 @@ using namespace oeng;
 static void LoadGame(Engine& e)
 {
 	auto& world = e.GetWorld();
+	
 	auto& cube = world.SpawnActor<AActor>();
 	auto& cube_mesh = cube.AddComponent<MeshComponent>();
-	cube_mesh.SetMesh("../Engine/Assets/Cube.omesh");
+	cube_mesh.SetMesh("../Assets/Cube.omesh");
 	cube.SetRootComponent(&cube_mesh);
 	cube.SetTransform({
 		{200, 75, 0},
-		Quat{UVec3::up, 225_deg} * Quat{UVec3::right, 90_deg},
+		Quat{UVec3::up, 225_deg} * Quat{UVec3::right, -90_deg},
 		{All{}, 100}}
 	);
+
+	auto& sphere = world.SpawnActor<AActor>();
+	auto& sphere_mesh = sphere.AddComponent<MeshComponent>();
+	sphere_mesh.SetMesh("../Assets/Sphere.omesh");
+	sphere.SetRootComponent(&sphere_mesh);
+	sphere.SetTransform({ {200, -75, 0}, {}, {All{}, 3} });
 }
 
 int main()
@@ -29,7 +36,6 @@ int main()
 	}
 	catch (const std::exception& e)
 	{
-		log::Critical("Unhandled exception: {}", e.what());
-		throw;
+		log::Critical(e.what());
 	}
 }
