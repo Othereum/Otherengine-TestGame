2a6bf2f2af17d637bd7bb8de89be5e064b02aed5
Othereum <seokjin.dev@gmail.com>
2020-06-10 22:16:14 +0900

    feat: Move around!

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,10 +1,71 @@
+#include <iostream>
+
 #include "Engine.hpp"
 #include "Log.hpp"
-
 #include "GameFramework/Actor.hpp"
+#include "Components/CameraComponent.hpp"
 #include "Components/MeshComponent.hpp"
+#include "Components/InputComponent.hpp"
+#include "GameFramework/MovementComponent.hpp"
+#include "SDL2/SDL_keycode.h"
 
 using namespace oeng;
+
+class SimplePawn : public AActor
+{
+public:
+	explicit SimplePawn(World& world)
+		:AActor{world},
+		movement_{AddComponent<MovementComponent>()},
+		camera_{AddComponent<CameraComponent>()}
+	{
+		SetRootComponent(&camera_);
+		camera_.Activate();
+
+		auto& input = AddComponent<InputComponent>();
+		input.BindAxis("MoveForward", [this](Float f) { MoveForward(f); });
+		input.BindAxis("MoveRight", [this](Float f) { MoveRight(f); });
+		input.BindAxis("MoveUp", [this](Float f) { MoveUp(f); });
+		input.BindAxis("Turn", [this](Float f) { Turn(f); });
+		input.BindAxis("LookUp", [this](Float f) { LookUp(f); });
+		
+		movement_.SetMaxSpeed(1);
+	}
+
+private:
+	void MoveForward(Float f) noexcept
+	{
+		if (!IsNearlyZero(f))
+			movement_.AddInput(*GetForward() * f);
+	}
+
+	void MoveRight(Float f) noexcept
+	{
+		if (!IsNearlyZero(f))
+			movement_.AddInput(*GetRight() * f);
+	}
+
+	void MoveUp(Float f) noexcept
+	{
+		if (!IsNearlyZero(f))
+			movement_.AddInput(Vec3::up * f);
+	}
+
+	void Turn(Float f) noexcept
+	{
+		if (!IsNearlyZero(f))
+			SetRot(Quat{UVec3::up, 1_deg * f} * GetRot());
+	}
+
+	void LookUp(Float f) noexcept
+	{
+		if (!IsNearlyZero(f))
+			SetRot(Quat{GetRight(), 0.75_deg * f} * GetRot());
+	}
+
+	MovementComponent& movement_;
+	CameraComponent& camera_;
+};
 
 static void LoadGame(Engine& e)
 {
@@ -14,17 +75,41 @@ static void LoadGame(Engine& e)
 	auto& cube_mesh = cube.AddComponent<MeshComponent>();
 	cube_mesh.SetMesh("../Assets/Cube.omesh");
 	cube.SetRootComponent(&cube_mesh);
-	cube.SetTransform({
+	cube.SetTrsf({
 		{200, 75, 0},
 		Quat{UVec3::up, 225_deg} * Quat{UVec3::right, -90_deg},
-		{All{}, 100}}
-	);
+		{All{}, 100}
+	});
 
 	auto& sphere = world.SpawnActor<AActor>();
 	auto& sphere_mesh = sphere.AddComponent<MeshComponent>();
 	sphere_mesh.SetMesh("../Assets/Sphere.omesh");
 	sphere.SetRootComponent(&sphere_mesh);
-	sphere.SetTransform({ {200, -75, 0}, {}, {All{}, 3} });
+	sphere.SetTrsf({ {200, -75, 0}, {}, {All{}, 3} });
+
+	auto& is = e.GetInputSystem();
+	is.AddAxis("MoveForward", {
+		{'w', InputType::kKeyboard, 1},
+		{'s', InputType::kKeyboard, -1},
+	});
+	is.AddAxis("MoveRight", {
+		{'a', InputType::kKeyboard, -1},
+		{'d', InputType::kKeyboard, 1},
+	});
+	is.AddAxis("Turn", {
+		{SDLK_RIGHT, InputType::kKeyboard, 1},
+		{SDLK_LEFT, InputType::kKeyboard, -1}
+	});
+	is.AddAxis("LookUp", {
+		{SDLK_UP, InputType::kKeyboard, -1},
+		{SDLK_DOWN, InputType::kKeyboard, 1},
+	});
+	is.AddAxis("MoveUp", {
+		{SDLK_LSHIFT, InputType::kKeyboard, -1},
+		{' ', InputType::kKeyboard, 1},
+	});
+	
+	auto& pawn = world.SpawnActor<SimplePawn>();
 }
 
 int main()
